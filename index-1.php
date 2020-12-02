d98c3ab2bcf77b871e76c646e7566bd61ea26cbe
gerald-gt <75348576+gerald-gt@users.noreply.github.com>
2020-12-02 13:09:31 +0700

    Add files via upload

---
+++ index-1.php
@@ -0,0 +1,6 @@
+﻿<?php
+/* This will give an error. Note the output
+ * above, which is before the header() call */
+header('Location: index.php');
+exit;
+?>
---
+++ top_links2.php
@@ -0,0 +1,11 @@
+<!-- Top Part Start-->
+<div class="top-links">
+	<div class="links"> 
+  		<a href="index-1.php">Home</a>|
+    	<a href="#">Shopping Cart</a>|
+    	<a href="#">Checkout</a>|
+    	<a href="#" target="_blank">Order History</a>|
+		<a href="#" onClick="showSearch();">Search</a>
+	</div>
+</div>
+<!-- Top Part End-->
